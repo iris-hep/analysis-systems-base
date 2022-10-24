6c942ed5ba45bb5293c8a1f7ec105ef49e1b1a12
Matthew Feickert <matthew.feickert@cern.ch>
2022-10-24 15:24:25 -0500

    Add noxfile commands

--- noxfile.py
+++ noxfile.py
@@ -13,11 +13,41 @@ nox.options.sessions = ["lock"]
 
 DIR = Path(__file__).parent.resolve()
 
+
 @nox.session(reuse_venv=True)
 def lock(session):
     """
-    Lint with pre-commit.
     Build a lockfile for the image with conda-lock
     """
     session.install("--upgrade", "conda-lock")
     # session.run("pre-commit", "run", "--all-files", *session.posargs)
+    session.run("conda-lock", "--help")
+    session.run("conda-lock", "lock", "--help")
+    session.run("conda-lock", "install", "--help")
+    session.run(
+        "conda-lock",
+        "lock",
+        "--platform",
+        "linux-64",
+        "--file",
+        "docker/environment.yml",
+        "--lockfile",
+        "docker/conda-lock.yml",
+    )
+
+
+@nox.session()
+def build(session):
+    """
+    Build image
+    """
+    session.run(
+        "docker",
+        "build",
+        "--file",
+        "docker/Dockerfile",
+        "--tag",
+        "iris-hep/analysis-systems-base:latest",
+        "docker",
+        external=True
+    )
