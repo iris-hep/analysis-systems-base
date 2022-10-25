03c48808f30c28ee0dbcd8b071d2c11b6ffa8ab4
Matthew Feickert <matthew.feickert@cern.ch>
2022-10-24 19:24:46 -0500

    Add pip-tools step

---
+++ docker/compile_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python -m venv /venv
+. /venv/bin/activate
+# Ensure pip-tools is installed
+python -m pip show pip-tools &> /dev/null
+if [ "$?" == "1" ]; then
+    python -m pip install --upgrade pip setuptools wheel
+    python -m pip install --upgrade 'pip-tools>=6.5.0'
+fi
+
+cd docker
+pip-compile \
+    --generate-hashes \
+    --output-file _requirements.lock \
+    requirements.txt
