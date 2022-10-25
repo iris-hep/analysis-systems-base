0730d2c4b848f9175a48468f245f06d2a790a252
Matthew Feickert <matthew.feickert@cern.ch>
2022-10-25 15:04:19 -0500

    feat: Add nox 'tag' and 'publish' sessions (#3)
    * For hub.opensciencegrid.org/iris-hep/analysis-systems-base repository

--- noxfile.py
+++ noxfile.py
@@ -74,3 +74,32 @@ def build(session):
         "docker",
         external=True,
     )
+
+
+@nox.session()
+def tag(session):
+    """
+    Tag images
+    """
+    for tag in ["latest", datetime.now().strftime("%Y-%m-%d")]:
+        session.run(
+            "docker",
+            "tag",
+            f"iris-hep/analysis-systems-base:{tag}",
+            f"hub.opensciencegrid.org/iris-hep/analysis-systems-base:{tag}",
+            external=True,
+        )
+
+
+@nox.session()
+def publish(session):
+    """
+    Push images to container registries
+    """
+    for tag in ["latest", datetime.now().strftime("%Y-%m-%d")]:
+        session.run(
+            "docker",
+            "push",
+            f"hub.opensciencegrid.org/iris-hep/analysis-systems-base:{tag}",
+            external=True,
+        )
